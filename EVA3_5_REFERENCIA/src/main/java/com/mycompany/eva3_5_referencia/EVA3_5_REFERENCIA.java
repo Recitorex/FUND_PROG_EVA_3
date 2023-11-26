d83b2bbe03b2bf183adb5c5d722bd5586e9db765
Recitorex <143562580+Recitorex@users.noreply.github.com>
2023-11-25 21:49:18 -0600

    Add files via upload

---
+++ EVA3_5_REFERENCIA/src/main/java/com/mycompany/eva3_5_referencia/EVA3_5_REFERENCIA.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_5_referencia;
+
+/**
+ *
+ * @author mayen
+ */
+public class EVA3_5_REFERENCIA {
+
+    public static void main(String[] args) {
+     int[] arreglo = new int[10];
+        System.out.println(arreglo); 
+    }
+}
